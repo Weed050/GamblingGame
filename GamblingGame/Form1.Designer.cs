67ed8cb16e217e549647cd5af0582591ca39ece6
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-20 14:50:56 +0100

    initialized grey hounds and fixed pictureBoxes

--- GamblingGame/Form1.Designer.cs
+++ GamblingGame/Form1.Designer.cs
@@ -28,16 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            raceTrackPictureBox = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
             pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)raceTrackPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
+            // raceTrackPictureBox
+            // 
+            raceTrackPictureBox.Location = new Point(12, 67);
+            raceTrackPictureBox.Name = "raceTrackPictureBox";
+            raceTrackPictureBox.Size = new Size(776, 303);
+            raceTrackPictureBox.TabIndex = 1;
+            raceTrackPictureBox.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(28, 157);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(125, 62);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(28, 225);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(125, 62);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(28, 293);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(125, 62);
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(108, 119);
+            pictureBox1.Location = new Point(28, 89);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(125, 62);
-            pictureBox1.TabIndex = 0;
+            pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
             // Form1
@@ -46,14 +86,25 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(pictureBox1);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(raceTrackPictureBox);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)raceTrackPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
+        private PictureBox raceTrackPictureBox;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
         private PictureBox pictureBox1;
     }
 }
--- GamblingGame/Form1.cs
+++ GamblingGame/Form1.cs
@@ -5,6 +5,36 @@ namespace GamblingGame
         public Form1()
         {
             InitializeComponent();
+            GreyHound[] GreyHoundArray = new GreyHound[4];
+            Random MyRandomizer = new Random();
+            GreyHoundArray[0] = new GreyHound()
+            {
+                MyPictureBox = pictureBox1,
+                StartingPosition = pictureBox1.Left,
+                RaceTrackLenght = raceTrackPictureBox.Width - pictureBox1.Width,
+                //Randomizer = MyRandomizer
+            };
+            GreyHoundArray[1] = new GreyHound()
+            {
+                MyPictureBox = pictureBox2,
+                StartingPosition = pictureBox2.Left,
+                RaceTrackLenght = raceTrackPictureBox.Width - pictureBox2.Width,
+                //Randomizer = MyRandomizer
+            };
+            GreyHoundArray[2] = new GreyHound()
+            {
+                MyPictureBox = pictureBox3,
+                StartingPosition = pictureBox3.Left,
+                RaceTrackLenght = raceTrackPictureBox.Width - pictureBox3.Width,
+                //Randomizer = MyRandomizer
+            };
+            GreyHoundArray[3] = new GreyHound()
+            {
+                MyPictureBox = pictureBox4,
+                StartingPosition = pictureBox4.Left,
+                RaceTrackLenght = raceTrackPictureBox.Width - pictureBox4.Width,
+                //Randomizer = MyRandomizer
+            };
         }
     }
 }
