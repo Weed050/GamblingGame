6d0180e0e29ec210f29728922dfdcd891ee71698
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-20 14:29:50 +0100

    lab page 228. Created class GreyHound that moves foward and return if it's complete it's run.

---
+++ GamblingGame/GreyHound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingGame
+{
+    public class GreyHound
+    {
+        public int StartingPosition;
+        public int RaceTrackLenght;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random MyRandom;
+        public bool Run()
+        {
+            int rand = MyRandom.Next(5);
+            Location += rand;
+            MyPictureBox.Left += rand;
+            if (RaceTrackLenght <= Location)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;    
+            }
+        }
+        public void TakeStartingPoint()
+        {
+            Location = 0;
+            Location = StartingPosition;
+        }
+    }
+}
